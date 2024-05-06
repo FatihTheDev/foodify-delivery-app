66cd4390a36b79c60773e9d123ae949d11ee7c27
FatihTheG <fatihtheg123@gmail.com>
2024-05-06 21:32:17 +0200

    Fixed some hover styles

--- foodify/app/components/Nav.tsx
+++ foodify/app/components/Nav.tsx
@@ -23,25 +23,25 @@ export default function Nav(){
         </Link>
 
         <ul className="large-navbar hidden lg:flex lg:justify-center lg:items-center mr-6">
-            <Link href="/about" className="p-4 hover:text-gray-400">About Us</Link>
-            <Link href="/contact"className="p-4 hover:text-gray-400">Contact</Link>
-            <Link href="/cart" className="p-4 hover:text-gray-400">Your Cart</Link>
-            <Link href="/settings"className="p-4 hover:text-gray-400">Settings</Link>
+            <Link href="/about" className="p-4 hover:text-gray-400 transition-all duration-500">About Us</Link>
+            <Link href="/contact"className="p-4 hover:text-gray-400 duration-500">Contact</Link>
+            <Link href="/cart" className="p-4 hover:text-gray-400 duration-500">Your Cart</Link>
+            <Link href="/settings"className="p-4 hover:text-gray-400 duration-500">Settings</Link>
         </ul>
 
-        <div className="burger-menu flex sm:flex lg:hidden">
+        <div className="burger-menu flex lg:hidden">
 
-        <svg onClick={showNav} className="w-7 h-7 mr-6 focus:bg-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+        <svg onClick={showNav} className="w-7 h-7 mr-6 focus:bg-gray-300 hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
-        <section className="absolute top-9 right-1 bg-yellow-200 rounded-md md:top-9 xs:right-[10%]  shadow-md">
-            <Link href="/about"><option className={navVisible ? "block p-3" : "hidden"} value="about">About</option></Link>
+        <section className="absolute top-9 right-1 bg-yellow-100 rounded-md md:top-9 xs:right-[10%]  shadow-md">
+            <Link href="/about"><option className={navVisible ? "block p-3 hover:text-gray-400 transition-all duration-300" : "hidden"} value="about">About</option></Link>
             <hr />
-            <Link href="/contact"><option className={navVisible ? "block p-3" : "hidden"} value="contact">Contact</option></Link>
+            <Link href="/contact"><option className={navVisible ? "block p-3 transition-all duration-500" : "hidden"} value="contact">Contact</option></Link>
             <hr />
-            <Link href="/cart"><option className={navVisible ? "block p-3" : "hidden"} value="cart">Your Cart</option></Link>
+            <Link href="/cart"><option className={navVisible ? "block p-3 transision-all duration-500" : "hidden"} value="cart">Your Cart</option></Link>
             <hr />
-            <Link href="/settings"><option className={navVisible ? "block p-3" : "hidden"} value="settings">Settings</option></Link>
+            <Link href="/settings"><option className={navVisible ? "block p-3 transition-all duration-500" : "hidden"} value="settings">Settings</option></Link>
         </section>
 
         </div>
